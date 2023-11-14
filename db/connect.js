784c276ad9bc6bd459be2462bc3464be13fd61f2
Bhargav <bhargavnadipineni@gmail.com>
2023-11-15 01:02:19 +0530

    Updated DB part

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql");
 
 const mysqlConnection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "clg-testdb",
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DBNAME
 });
 
 module.exports = mysqlConnection;
