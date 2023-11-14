0e8518b00a9334cdc50640c6e8da1bb15129dfa8
Bhargav <bhargavnadipineni@gmail.com>
2023-11-15 01:16:10 +0530

    Updated DB part

--- public/main.js
+++ public/main.js
@@ -4,9 +4,9 @@ const userContainer = document.querySelector(".userControl");
 const adminContainer = document.querySelector(".adminControl");
 
 userContainer.onclick = function () {
-  window.open("http://127.0.0.1:8080/map.html", "_self");
+  window.open("https://cloudprojectserver.onrender.com/map.html", "_self");
 };
 
 adminContainer.onclick = function () {
-  window.open("http://127.0.0.1:8080/login.html", "_self");
+  window.open("https://cloudprojectserver.onrender.com/login.html", "_self");
 };
