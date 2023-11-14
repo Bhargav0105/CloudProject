b4a6e954fac4211e3d170cd657cd0ae984e8e599
Bhargav <bhargavnadipineni@gmail.com>
2023-11-15 00:06:30 +0530

    Updated DB part

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const mysqlConnection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "clg-testdb", 
+  host: "locab54jmicytyrxx8ffsgh7-mysql.services.clever-cloud.comlhost",
+  user: "uu0shkcv0fhudwr0",
+  password: "ws9dTZJCgFM17KIDoDiv",
+  database: "b54jmicytyrxx8ffsgh7", 
 });
 
 module.exports = mysqlConnection;
